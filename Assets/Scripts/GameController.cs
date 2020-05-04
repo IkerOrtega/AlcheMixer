5d4adb41a3a378eb309d27edd99fe780e675c33e
Iker.ortega <iker.ortega.g@gmail.com>
2020-05-04 23:44:59 +0200

    Added wheel witn configurable sections and spoon movement.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public int initial_sections = 8;
+
+    [HideInInspector]
+    public float ANGLE_STEP = 45;
+    [HideInInspector]
+    public int NUM_SECTIONS = 8;
+
+
+    private void Awake()
+    {
+       UpdateNumSections(initial_sections);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void UpdateNumSections(int sections) {
+        if(NUM_SECTIONS != sections) {
+            NUM_SECTIONS = sections;
+            ANGLE_STEP = Constants.MAX_ANGLE / NUM_SECTIONS;
+        }
+    }
+}
---
+++ Assets/Scripts/SpoonController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpoonController : MonoBehaviour
+{
+    public GameObject ingredientPrefab;
+    public float ingRadius = 0.5f;
+
+    private GameController _gc;
+
+    private Vector2 direction;
+    private List<GameObject> ingList;
+    private int sectionSelected = 0;
+
+
+
+
+
+    private void Awake()
+    {
+        _gc = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+        ingList = new List<GameObject>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (ingredientPrefab != null)
+        {
+            for (int i = 0; i < _gc.NUM_SECTIONS; i++)
+            {
+                Quaternion rotation = Quaternion.AngleAxis(_gc.ANGLE_STEP * i, Vector3.forward);
+                Vector3 direction = rotation * Vector2.right;
+                Vector3 position = transform.position + (direction * ingRadius);
+                ingList.Add(Instantiate(ingredientPrefab, position, rotation));
+            }
+           
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        // TODO: Not snapping correctly to diagonals.
+        // TODO Get input from mouse.
+        if (horizontalInput != 0 || verticalInput != 0)
+        {
+            float tempRotation = Mathf.Atan2(verticalInput, horizontalInput) * Mathf.Rad2Deg;
+            if(tempRotation < 0f) {
+                tempRotation = Constants.MAX_ANGLE + tempRotation;
+            }
+            //Snap to nearest ANGLE_STEP
+            
+            sectionSelected = Mathf.RoundToInt(tempRotation / _gc.ANGLE_STEP);
+
+            // Debug.Log("Direction: (" + horizontalInput + ", " + verticalInput + ")");
+            
+            transform.rotation = Quaternion.AngleAxis(_gc.ANGLE_STEP * sectionSelected, Vector3.forward);
+            Debug.Log("Section Selected: " + sectionSelected);
+            Debug.Log("Rotation: " + _gc.ANGLE_STEP * sectionSelected);
+        }
+
+       
+
+    }
+
+    private void FixedUpdate()
+    {
+
+
+    }
+
+    private void LateUpdate()
+    {
+        for (int i = 0; i < ingList.Count; i++)
+        {
+            ingList[i].GetComponent<SpriteRenderer>().color = (i == sectionSelected ? Color.yellow : Color.white);
+        }
+
+    }
+}
